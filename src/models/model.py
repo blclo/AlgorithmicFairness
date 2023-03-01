0c32c6a247bb140880a723245c2c59d39a44e94d
blclo <blclo@github.com>
2023-03-01 14:51:08 +0100

    general train architecture imported from old project

---
+++ src/models/model.py
@@ -0,0 +1,109 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision.transforms as transforms
+
+
+class CNN(nn.Module):
+    """Convolutional Neural Network.
+    
+    We provide a simple network with a Conv layer, followed by pooling,
+    and a fully connected layer. Modify this to test different architectures,
+    and hyperparameters, i.e. different number of layers, kernel size, feature
+    dimensions etc.
+
+    See https://pytorch.org/docs/stable/nn.html for a list of different layers
+    in PyTorch.
+    """
+
+    def __init__(self):
+        """Initialize layers. defines the model architecture but it's executed in the forward pass"""
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(8, 8)
+        self.conv2 = nn.Conv2d(6, 6, 3, padding=2)
+        self.act = nn.ReLU()
+        self.fc1 = nn.Linear(294, 36)
+        self.fc2 = nn.Linear(36, 6)
+        self.dropout = nn.Dropout(0.3)
+
+    def forward(self, x):
+        """Forward pass of network."""
+        """
+        x = self.pool(self.act(self.conv1(x)))
+        x = self.conv2(x)
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = self.dropout(x)
+        x = self.fc1(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        #x = self.dropout(x)
+        x = self.fc3(x)
+        """
+        """
+        85.75%
+        epochs = 15 without data aug
+        """
+        x = self.pool(self.act(self.conv1(x)))
+        x = self.conv2(x)
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = self.dropout(x)
+        x = self.act(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x
+
+
+def get_transforms_train():
+    """Return the transformations applied to images during training.
+    
+    See https://pytorch.org/vision/stable/transforms.html for a full list of 
+    available transforms.
+    """
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),  # convert image to a PyTorch Tensor
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),  # normalize
+            transforms.RandomHorizontalFlip(p=1),
+            transforms.CenterCrop(50),
+            transforms.ColorJitter(brightness=0,contrast=0,saturation=0,hue=0)    
+        ]
+    )
+    return transform
+
+
+def get_transforms_val():
+    """Return the transformations applied to images during validation.
+
+    Note: You do not need to change this function 
+    """
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),  # convert image to a PyTorch Tensor
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # normalize
+        ]
+    )
+    return transform
+
+
+def get_loss_function():
+    """Return the loss function to use during training. We use
+       the Cross-Entropy loss for now.
+    
+    See https://pytorch.org/docs/stable/nn.html#loss-functions.
+    """
+    return nn.CrossEntropyLoss()
+
+
+def get_optimizer(network, lr=0.001, momentum=0.9):
+    """Return the optimizer to use during training.
+    
+    network specifies the PyTorch model.
+
+    See https://pytorch.org/docs/stable/optim.html#how-to-use-an-optimizer.
+    """
+
+    # The fist parameter here specifies the list of parameters that are
+    # learnt. In our case, we want to learn all the parameters in the network
+    return optim.SGD(network.parameters(), lr=lr, momentum=momentum)
---
+++ src/models/train_model.py
@@ -0,0 +1,120 @@
+from cnn_network import CNN, get_loss_function, get_optimizer
+from cnn_network import get_transforms_val, get_transforms_train, CNN, get_loss_function, get_optimizer
+import torch
+import torchvision
+
+cnn_network = CNN()  # create CNN model
+
+print("CNN Architecture:")
+print(cnn_network)
+
+print("")
+print("Testing network input / output.")
+test_input = torch.zeros(1, 3, 50, 50)  # batch size X 3 (RGB) X width X height
+print(f"Input shape: {test_input.shape}")
+test_output = cnn_network(test_input)  # batch size X # classes
+print(f"Output shape: {test_output.shape}")
+
+criterion = get_loss_function()  # get loss function
+optimizer = get_optimizer(cnn_network, lr=0.001, momentum=0.9)  # get optimizer
+
+# *********************************************************** #
+# set all training parameters. You can play around with these
+# *********************************************************** #
+
+batch_size = 8          # Number of images in each batch
+learning_rate = 0.001    # Learning rate in the optimizer
+momentum = 0.9           # Momentum in SGD
+num_epochs = 15           # Number of passes over the entire dataset
+print_every_iters = 100  # Print training loss every X mini-batches
+
+
+# *********************************************************** #
+# Initialize all the training components, e.g. model, cost function
+# *********************************************************** #
+
+# Get transforms
+transform_train = get_transforms_train()
+transform_val = get_transforms_val()
+
+# Generate our data loaders
+train_loader, val_loader, test_loader = get_dataloaders(batch_size, transform_train, transform_val)
+
+# create CNN model
+cnn_network = CNN()  
+
+# Get optimizer and loss functions
+criterion = get_loss_function() 
+optimizer = get_optimizer(cnn_network, lr=learning_rate, momentum=momentum) 
+
+# *********************************************************** #
+# The main training loop. You dont need to change this
+# *********************************************************** #
+training_loss_per_epoch = []
+val_loss_per_epoch = []
+for epoch in range(num_epochs):  # loop over the dataset multiple times
+    # First we loop over training dataset
+    running_loss = 0.0
+
+    # Set network to train mode before training
+    cnn_network.train()
+    for i, data in enumerate(train_loader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()  # zero the gradients from previous iteration
+
+        # forward + backward + optimize
+        outputs = cnn_network(inputs)  # forward pass to obtain network outputs
+        loss = criterion(outputs, labels)  # compute loss with respect to labels
+        loss.backward()  # compute gradients with backpropagation (autograd)
+        optimizer.step()  # optimize network parameters
+
+        # print statistics
+        running_loss += loss.item()
+        if (i + 1) % print_every_iters == 0:
+            print(
+                f'[Epoch: {epoch + 1} / {num_epochs},'
+                f' Iter: {i + 1:5d} / {len(train_loader)}]'
+                f' Training loss: {running_loss / (i + 1):.3f}'
+            )
+    
+    mean_loss = running_loss / len(train_loader)
+    training_loss_per_epoch.append(mean_loss)
+
+    # Next we loop over validation dataset
+    running_loss = 0.0
+
+    # Set network to eval mode before validation
+    cnn_network.eval()
+    for i, data in enumerate(val_loader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # on validation dataset, we only do forward, without computing gradients
+        with torch.no_grad():
+            outputs = cnn_network(inputs)  # forward pass to obtain network outputs
+            loss = criterion(outputs, labels)  # compute loss with respect to labels
+        
+        # print statistics
+        running_loss += loss.item()
+
+    mean_loss = running_loss / len(val_loader)
+    val_loss_per_epoch.append(mean_loss)
+
+    print(
+        f'[Epoch: {epoch + 1} / {num_epochs}]'
+        f' Validation loss: {mean_loss:.3f}'
+    )
+
+print('Finished Training')
+
+# Plot the training curves
+plt.figure()
+plt.plot(np.array(training_loss_per_epoch))
+plt.plot(np.array(val_loss_per_epoch))
+plt.legend(['Training loss', 'Val loss'])
+plt.xlabel('Epoch')
+plt.show()
+plt.close()
