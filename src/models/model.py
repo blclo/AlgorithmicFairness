09b682679605a14c416beb959fec764134ce3d1f
blclo <blclo@github.com>
2023-03-02 19:36:36 +0100

    removed <<<

--- src/models/model.py
+++ src/models/model.py
@@ -4,78 +4,6 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torchvision.transforms as transforms
 
-<<<<<<< HEAD
-
-class CNN(nn.Module):
-    """Convolutional Neural Network.
-    
-    We provide a simple network with a Conv layer, followed by pooling,
-    and a fully connected layer. Modify this to test different architectures,
-    and hyperparameters, i.e. different number of layers, kernel size, feature
-    dimensions etc.
-
-    See https://pytorch.org/docs/stable/nn.html for a list of different layers
-    in PyTorch.
-    """
-
-    def __init__(self):
-        """Initialize layers. defines the model architecture but it's executed in the forward pass"""
-        super().__init__()
-        
-        self.fc1 = nn.Linear(294, 36)
-        self.act = nn.ReLU()
-        self.fc2 = nn.Linear(36, 6)
-        
-        self.dropout = nn.Dropout(0.3)
-
-    def forward(self, x):
-        """Forward pass of network."""
-        """
-        85.75%
-        epochs = 15 without data aug
-        """
-        x = self.pool(self.act(self.conv1(x)))
-        x = self.conv2(x)
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
-        x = self.dropout(x)
-        x = self.act(self.fc1(x))
-        x = self.dropout(x)
-        x = self.fc2(x)
-        return x
-
-
-def get_transforms_train():
-    """Return the transformations applied to images during training.
-    
-    See https://pytorch.org/vision/stable/transforms.html for a full list of 
-    available transforms.
-    """
-    transform = transforms.Compose(
-        [
-            transforms.ToTensor(),  # convert image to a PyTorch Tensor
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),  # normalize
-            transforms.RandomHorizontalFlip(p=1),
-            transforms.CenterCrop(50),
-            transforms.ColorJitter(brightness=0,contrast=0,saturation=0,hue=0)    
-        ]
-    )
-    return transform
-
-
-def get_transforms_val():
-    """Return the transformations applied to images during validation.
-
-    Note: You do not need to change this function 
-    """
-    transform = transforms.Compose(
-        [
-            transforms.ToTensor(),  # convert image to a PyTorch Tensor
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # normalize
-        ]
-    )
-    return transform
-
-=======
 #  ---------------  Model  ---------------
 class MLP(nn.Module):
     def __init__(self, D_in, H=15, D_out=1):
@@ -88,7 +16,6 @@ class MLP(nn.Module):
         x = self.relu(x)
         x = self.fc2(x)
         return x.squeeze()
->>>>>>> main
 
 def get_loss_function():
     """Return the loss function to use during training. We use
